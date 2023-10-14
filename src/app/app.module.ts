5f8cda891fdf687671a79fef00cecd784383b217
Krzysztof Bartczak <krzysztof@bartczak.eu>
2023-10-14 15:14:53 +0200

    initialize basic layout with header, footer, and crypto-list components
    Signed-off-by: Krzysztof Bartczak <krzysztof@bartczak.eu>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+import { HomeComponent } from "./pages/home/home.component";
+
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { CryptoListComponent } from "./components/crypto-list/crypto-list.component";
+
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+    declarations: [AppComponent, HeaderComponent, FooterComponent, CryptoListComponent, HomeComponent],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule],
     providers: [],
     bootstrap: [AppComponent]
 })
