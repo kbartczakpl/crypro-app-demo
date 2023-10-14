e8c6f76ac8682f1c269557f2e41353bd3ba1b17b
Krzysztof Bartczak <krzysztof@bartczak.eu>
2023-10-14 16:48:53 +0200

    implement CryptoService add related models and env config
    Signed-off-by: Krzysztof Bartczak <krzysztof@bartczak.eu>

---
+++ src/app/models/crypto.model.ts
@@ -0,0 +1,9 @@
+export interface Crypto {
+    id: string;
+    api_symbol: string;
+    name: string;
+    symbol: string;
+    large?: string;
+    market_cap_rank?: number;
+    thumb?: string;
+}
