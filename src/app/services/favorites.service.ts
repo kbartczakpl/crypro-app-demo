4e68b92919afae3eab3e972fe94e7ff50c5aa5d1
Krzysztof Bartczak <krzysztof@bartczak.eu>
2023-10-14 17:46:25 +0200

    favorites service store data in localStorage
    Signed-off-by: Krzysztof Bartczak <krzysztof@bartczak.eu>

--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -10,14 +10,32 @@ export class FavoritesService {
   private favoritesSubject = new BehaviorSubject<Crypto[]>(this.favoritesList);
   favorites$: Observable<Crypto[]> = this.favoritesSubject.asObservable();
 
+  constructor() {
+    this.loadFavoritesFromStorage();
+  }
+
+  private loadFavoritesFromStorage(): void {
+    const storedFavorites = localStorage.getItem('favorites');
+    if (storedFavorites) {
+      this.favoritesList = JSON.parse(storedFavorites);
+      this.favoritesSubject.next(this.favoritesList);
+    }
+  }
+
+  private saveFavoritesToStorage(): void {
+    localStorage.setItem('favorites', JSON.stringify(this.favoritesList));
+  }
+
   addToFavorites(crypto: Crypto): void {
     this.favoritesList.push(crypto);
     this.favoritesSubject.next(this.favoritesList);
+    this.saveFavoritesToStorage();
   }
 
   removeFromFavorites(cryptoId: string): void {
     this.favoritesList = this.favoritesList.filter(c => c.id !== cryptoId);
     this.favoritesSubject.next(this.favoritesList);
+    this.saveFavoritesToStorage();
   }
 
   getFavorites(): Observable<Crypto[]> {
